29f64c55712ab7c7facf61009e21493743aa56d1
Wagner.Araujo <araujowp@gmail>
2022-04-22 07:44:36 -0300

    JSTL - Expression Language

--- src/br/com/alura/gerenciador/servlet/Empresa.java
+++ src/br/com/alura/gerenciador/servlet/Empresa.java
@@ -1,9 +1,12 @@
 package br.com.alura.gerenciador.servlet;
 
+import java.util.Date;
+
 public class Empresa {
 
 	int id;
 	String nome;
+	private Date dataAbertura = new Date();
 
 	public int getId() {
 		return id;
@@ -19,5 +22,13 @@ public class Empresa {
 	
 	public void setNome(String nome) {
 		this.nome = nome;
+	}
+	
+	public Date getDataAbertura() {
+		return dataAbertura;
+	}
+	
+	public void setDataAbertura(Date dataAbertura) {
+		this.dataAbertura = dataAbertura;
 	}
 }
