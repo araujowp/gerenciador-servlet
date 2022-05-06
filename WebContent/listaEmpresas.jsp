186946311e1ca3ed4f98cd4417a9aa377ad6029b
Wagner.Araujo <araujowp@gmail>
2022-05-06 09:35:07 -0300

    complementando o crud

--- WebContent/listaEmpresas.jsp
+++ WebContent/listaEmpresas.jsp
@@ -18,8 +18,12 @@
 
     <ul>
         <c:forEach items="${empresas}" var="empresa">
-            <li>${empresa.nome}  - <fmt:formatDate value="${empresa.dataAbertura}" pattern="dd/MM/yyyy" /> </li>
-        </c:forEach>
+			<li>${empresa.nome}- <fmt:formatDate
+					value="${empresa.dataAbertura}" pattern="dd/MM/yyyy" /> 
+					<a href="mostraEmpresa?id=${empresa.id}"> editar </a>&nbsp;
+					<a href="removeEmpresa?id=${empresa.id}"> remover </a>
+			</li>
+		</c:forEach>
     </ul>
 
 </body>
--- src/br/com/alura/gerenciador/servlet/Banco.java
+++ src/br/com/alura/gerenciador/servlet/Banco.java
@@ -1,22 +1,31 @@
 package br.com.alura.gerenciador.servlet;
 
 import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.List;
 
 public class Banco {
 
 	private static List<Empresa> lista = new ArrayList<Empresa>();
+	private static Integer chaveSequencial =1;
 	
     static {
         Empresa empresa = new Empresa();
+        empresa.setId(chaveSequencial++);
         empresa.setNome("Alura");
         Empresa empresa2 = new Empresa();
+        empresa2.setId(chaveSequencial++);
         empresa2.setNome("Caelum");
+        Empresa empresa3 = new Empresa();
+        empresa3.setId(chaveSequencial++);
+        empresa3.setNome("Facebook");
         lista.add(empresa);
         lista.add(empresa2);
+        lista.add(empresa3);
     }
 	
 	public void adiciona(Empresa empresa) {
+		empresa.setId(chaveSequencial++);
 		lista.add(empresa);
 	}
 	
@@ -24,4 +33,24 @@ public class Banco {
 		return lista;
 	}
 
+	public void removeEmpresa(Integer id) { 
+	    Iterator<Empresa> it = lista.iterator(); 
+	    
+	    while(it.hasNext()) {
+	    	Empresa empresa = it.next();
+	    	
+	    	if(empresa.getId() == id ) {
+	    		it.remove();
+	    	}
+	    }
+	}
+
+	public Empresa buscaEmpresaPorID(Integer id) {
+		
+		for(Empresa empresa: lista) {
+			if(empresa.getId() == id)
+				return empresa;
+		}
+		return null;
+	} 
 }
